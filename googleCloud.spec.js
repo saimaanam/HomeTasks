ef6856d51e5b1d8e21a047f71b73af8349d2e536
Saima Anam Syed <101432736+saimaanam@users.noreply.github.com>
2022-11-23 16:59:58 -0600

    Add files via upload

--- googleCloud.spec.js
+++ googleCloud.spec.js
@@ -2,7 +2,7 @@
 
 const homepage = require('../pages/home-page');
 const searchpage = require('../pages/search-page');
-const calculatorPage = require("../pages/calculator-page");
+const calculatorPage = require("../pages/calculator.page");
 const emailpage = require('../pages/email-page');
 const { getPrice } = require('../pages/email-page');
 
@@ -28,11 +28,11 @@ describe ('googleCloudSearch',()=>{
         await calculatorPage.passNumberOfInstance(4);
         await calculatorPage.osSelection();
         await calculatorPage.seriesSelection();
-        await calculatorPage.machineTypeSelection();
+        //await calculatorPage.machineTypeSelection();
         await calculatorPage.addGpuSelection();
         await calculatorPage.gpuTypeSelection();
-        await calculatorPage.numberOfGpuSelection();
-        await calculatorPage.SSDselection();
+        //await calculatorPage.numberOfGpuSelection();
+        //await calculatorPage.SSDselection();
         await calculatorPage.dataCentreLocationSelection();
         await calculatorPage.committedUsageSelection();
         
@@ -46,7 +46,7 @@ describe ('googleCloudSearch',()=>{
         await getPrice();
         await calculatorPage.selectEmail();
         await emailpage.openBrowserForEmail();
-    await browser.pause(5000);
+        await browser.pause(5000);
         await emailpage.selectEmailAddressAndSaveInVar();
 
         await emailpage.switchBrowser();
