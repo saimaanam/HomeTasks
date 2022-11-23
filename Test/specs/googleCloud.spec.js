4557561ffc4689d422643e448ffd33777dda3f0f
Saima Anam Syed <101432736+saimaanam@users.noreply.github.com>
2022-11-23 16:06:26 -0600

    Update googleCloud.spec.js

--- Test/specs/googleCloud.spec.js
+++ Test/specs/googleCloud.spec.js
@@ -2,7 +2,7 @@
 
 const homepage = require('../pages/home-page');
 const searchpage = require('../pages/search-page');
-const calculatorPage = require("../pages/calculator-page");
+const calculatorPage = require("../pages/calculator.page");
 const emailpage = require('../pages/email-page');
 const { getPrice } = require('../pages/email-page');
 
@@ -28,11 +28,11 @@ describe ('googleCloudSearch',()=>{
         await calculatorPage.passNumberOfInstance(4);
         await calculatorPage.osSelection();
         await calculatorPage.seriesSelection();
-        await calculatorPage.machineTypeSelection();
+        //await calculatorPage.machineTypeSelection();
         await calculatorPage.addGpuSelection();
         await calculatorPage.gpuTypeSelection();
-        await calculatorPage.numberOfGpuSelection();
-        await calculatorPage.SSDselection();
+        //await calculatorPage.numberOfGpuSelection();
+        //await calculatorPage.SSDselection();
         await calculatorPage.dataCentreLocationSelection();
         await calculatorPage.committedUsageSelection();
         
